557782ca62c164e088b63798067fd9c81d965d57
kylelingat <kylejlingat@gmail.com>
2017-01-11 08:59:22 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,19 +5,15 @@ console.log('connected');
 function generateFortuneCookie () {
 	var fortunesList = ['A beautiful, smart, and loving person will be coming into your life.','A dubious friend may be an enemy in camouflage.','A feather in the hand is better than a bird in the air.','The early bird gets the worm, but the second mouse gets the cheese.', 'Your road to glory will be rocky, but fulfilling.', 'Dont worry about money. The best things in life are free.', 'Dont pursue happiness, create it.', 'Courage is not the absence of fear; it is the conquest of it.', 'Nothing is so much to be feared as fear.'];
 	/* all possible fortunes */
-
 	var random = fortunesList[Math.floor(Math.random() * fortunesList.length)];
 	document.getElementById('text').textContent = random;
 	/* randomizer set to random variable, change textContent of fortune-cookie-text to the random variable value */
 	console.log(random)
-
 	var list = document.getElementById('someting');
 	var node = document.createElement("div");
 	node.innerHTML = random
 	list.appendChild(node);
 }
-
-
 function generateKermit () {
 	document.body.style.backgroundImage = "url('http://i0.kym-cdn.com/entries/icons/original/000/021/668/kermie.JPG')";
 	var kermitList = ['die', 'skip school', 'kick that ice under the fridge', 'late to class, might as well not even go school','spend all ur money', 'always overreact', 'make a scene']
@@ -26,8 +22,7 @@ function generateKermit () {
 	document.getElementById('someting').style.backgroundColor = 'white';
 	document.getElementById('text').style.color = 'green';
 	document.getElementById('text').textContent = random;
-	console.log('User ' + random)
-
+	console.log('User ' + random);
 	var list = document.getElementById('someting');
 	var node = document.createElement("div");
 	node.innerHTML = random
