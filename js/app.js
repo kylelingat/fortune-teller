ea2f7fa18d62873d0c57b712af56cf81fa0750f2
kylelingat <kylejlingat@gmail.com>
2017-01-11 08:58:21 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function generateFortuneCookie () {
 	var random = fortunesList[Math.floor(Math.random() * fortunesList.length)];
 	document.getElementById('text').textContent = random;
 	/* randomizer set to random variable, change textContent of fortune-cookie-text to the random variable value */
-	console.log('User ' + random)
+	console.log(random)
 
 	var list = document.getElementById('someting');
 	var node = document.createElement("div");
