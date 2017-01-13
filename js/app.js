f0e227d2cb882f93cec65fe55f6680adba6f7ae6
kylelingat <kylejlingat@gmail.com>
2017-01-13 08:20:00 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,6 +13,15 @@ function generateFortuneCookie () {
 	node.innerHTML = random
 	list.appendChild(node);		
 }
+
+
+
+
+
+
+
+
+
 function generateKermit () {
 	document.body.style.backgroundImage = "url('https://i0.kym-cdn.com/entries/icons/original/000/021/668/kermie.JPG')";
 	var kermitList = ['die', 'skip school', 'kick that ice under the fridge', 'late to class, might as well not even go school','spend all ur money', 'always overreact', 'make a scene']
