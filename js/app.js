aa47e710b861112db513db0acf895ee5d547a326
kylelingat <kylejlingat@gmail.com>
2017-03-31 08:38:28 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ console.log('connected');
 
 
 function generateFortuneCookie () {
-	var fortunesList = ['A beautiful, smart, and loving person will be coming into your life.','A dubious friend may be an enemy in camouflage.','A feather in the hand is better than a bird in the air.','The early bird gets the worm, but the second mouse gets the cheese.', 'Your road to glory will be rocky, but fulfilling.', 'Dont worry about money. The best things in life are free.', 'Dont pursue happiness, create it.', 'Courage is not the absence of fear; it is the conquest of it.', 'Nothing is so much to be feared as fear.'];
+	var fortunesList = ['die', 'skip school', 'kick that ice under the fridge', 'late to class, might as well not even go school','spend all ur money', 'always overreact', 'make a scene'];
 	/* all possible fortunes */
 	var random = fortunesList[Math.floor(Math.random() * fortunesList.length)];
 	document.getElementById('text').textContent = random;
@@ -12,19 +12,4 @@ function generateFortuneCookie () {
 	var node = document.createElement("div");
 	node.innerHTML = random
 	list.appendChild(node);		
-}
-function generateKermit () {
-	document.body.style.backgroundImage = "url('https://i0.kym-cdn.com/entries/icons/original/000/021/668/kermie.JPG')";
-	var kermitList = ['die', 'skip school', 'kick that ice under the fridge', 'late to class, might as well not even go school','spend all ur money', 'always overreact', 'make a scene']
-	var random = kermitList[Math.floor(Math.random() * kermitList.length)];
-	document.getElementById('text').style.backgroundColor = 'white';
-	document.getElementById('someting').style.backgroundColor = 'white';
-	document.getElementById('text').style.color = 'green';
-	document.getElementById('text').textContent = random;
-	console.log('User ' + random);
-	var list = document.getElementById('someting');
-	var node = document.createElement("div");
-	node.innerHTML = random
-	node.style.color = 'green';
-	list.appendChild(node);
 }
